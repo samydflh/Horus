d1d7ec0caeeb0476b6b68279503ac9628470f839
samydflh <samy.dflh@gmail.com>
2025-11-26 14:51:27 +0100

    feat(controls): add base controls

---
+++ horus_audit/controls/base/__init__.py
@@ -0,0 +1,14 @@
+from horus_audit.controls.base.filesystem import (
+    check_file_exists,
+    check_file_permissions
+)
+from horus_audit.controls.base.kernel import check_sysctl_value
+from horus_audit.controls.base.services import check_service_enabled
+
+
+__all__ = [
+    "check_file_exists",
+    "check_file_permissions",
+    "check_service_enabled",
+    "check_sysctl_value"
+]
---
+++ horus_audit/controls/base/kernel.py
@@ -0,0 +1,33 @@
+from horus_audit.core.executor import Executor
+from horus_audit.core.registry import register_control
+from horus_audit.core.result import ControlResult
+
+
+@register_control("check_sysctl_value")
+def check_sysctl_value(
+    key: str,
+    expected_value: str | int,
+    *,
+    executor: Executor,
+    control_name: str
+) -> ControlResult:
+    """
+    Check whether the value of a sysctl parameter match recommendations.
+
+    Returned status:
+    - `PASSED` if the sysctl parameter is compliant.
+    - `FAILED` if the sysctl parameter do not match recommendations.
+    - `SKIPPED` if the sysctl parameter is unreadable.
+    """
+
+    result = executor.run(f"sysctl -n {key}")
+
+    if result.code != 0:
+        return ControlResult.skipped_(control_name, f"Cannot read sysctl {key}")
+
+    sysctl_value = result.stdout.strip()
+
+    if str(expected_value) == sysctl_value:
+        return ControlResult.passed_(control_name, f"{key}={sysctl_value}")
+
+    return ControlResult.failed_(control_name, f"{key}={sysctl_value}, expected {expected_value}")
