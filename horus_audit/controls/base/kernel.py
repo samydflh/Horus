8e0b3bb62b49585242fbbb7fc0cd01d0a26d5a91
samydflh <samy.dflh@gmail.com>
2025-12-01 18:07:35 +0100

    refactor(kernel): implement module availability controls

--- horus_audit/controls/base/kernel.py
+++ horus_audit/controls/base/kernel.py
@@ -23,11 +23,81 @@ def check_sysctl_value(
     result = executor.run(f"sysctl -n {key}")
 
     if result.code != 0:
-        return ControlResult.skipped_(control_name, f"Cannot read sysctl {key}")
+        return ControlResult.skipped_(
+            control_name,
+            f"Cannot read sysctl {key}"
+        )
 
     sysctl_value = result.stdout.strip()
 
     if str(expected_value) == sysctl_value:
-        return ControlResult.passed_(control_name, f"{key}={sysctl_value}")
+        return ControlResult.passed_(
+            control_name,
+            f"{key}={sysctl_value}"
+        )
 
-    return ControlResult.failed_(control_name, f"{key}={sysctl_value}, expected {expected_value}")
+    return ControlResult.failed_(
+        control_name,
+        f"{key}={sysctl_value}, expected {expected_value}"
+    )
+
+
+@register_control("check_module_available")
+def check_module_available(
+    module: str,
+    *,
+    executor: Executor,
+    control_name: str
+) -> ControlResult:
+    """
+    Check whether a module is not available.
+
+    Returned status:
+    - `PASSED` if the module is not available.
+    - `FAILED` otherwise.
+    """
+
+    result = executor.run(f"modprobe -n {module}")
+
+    if result.code == 0:
+        return ControlResult.failed_(
+            control_name,
+            f"{module} module is available"
+        )
+
+    return ControlResult.passed_(
+        control_name,
+        f"{module} module is not available"
+    )
+
+
+@register_control("check_module_loadable")
+def check_module_loadable(
+    module: str,
+    *,
+    executor: Executor,
+    control_name: str
+) -> ControlResult:
+    """
+    Check whether a module is unloadable.
+
+    Returned status:
+    - `PASSED` if the module is unloadable.
+    - `FAILED` otherwise.
+    """
+
+    result = executor.run(
+        "find /lib/modules/**/kernel/ "
+        f"-type f -name '{module}*'"
+    )
+
+    if result.stdout:
+        return ControlResult.failed_(
+            control_name,
+            f"{module} module is loadable"
+        )
+
+    return ControlResult.passed_(
+        control_name,
+        f"{module} module is unloadable"
+    )
